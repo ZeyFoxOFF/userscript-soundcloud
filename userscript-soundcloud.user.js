8125499a48ca2d216caf3064b2b77b50fc26130f
ZeyFoxOFF <zeyfoxoff@gmail.com>
2025-12-13 22:31:41 +0100

    update 1.0.2 + Removed old css and added new css

--- userscript-soundcloud.user.js
+++ userscript-soundcloud.user.js
@@ -1,35 +1,23 @@
 // ==UserScript==
 // @name         Hide SoundCloud Visual Ads
 // @namespace    https://github.com/ZeyFoxOFF/userscript-soundcloud
-// @version      1.0.1
+// @version      1.0.2
 // @description  Hide visual ads for soundcloud
 // @author       ZeyFox
 // @match        *://soundcloud.com/*
 // @icon         https://i.ibb.co/QFQyyK3q/Icon.jpg
 // @grant        GM_addStyle
 // @homepageURL  https://github.com/ZeyFoxOFF/userscript-soundcloud
-// @updateURL    https://raw.githubusercontent.com/ZeyFoxOFF/userscript-soundcloud/refs/heads/main/userscript-soundcloud.user.js
-// @downloadURL  https://raw.githubusercontent.com/ZeyFoxOFF/userscript-soundcloud/refs/heads/main/userscript-soundcloud.user.js
 // @license MIT
+// @downloadURL https://update.greasyfork.org/scripts/554340/Hide%20SoundCloud%20Visual%20Ads.user.js
+// @updateURL https://update.greasyfork.org/scripts/554340/Hide%20SoundCloud%20Visual%20Ads.meta.js
 // ==/UserScript==
 
 (function() {
     'use strict';
 
     const CSS = `
-        .ouxtlr0 {
-            display: none;
-        }
-        .creatorBadge__custom {
-            display: none;
-        }
         .creatorSubscriptionsButton {
-            display: none;
-        }
-        .soundTitle__usernameHeroContainer {
-            display: none;
-        }
-        .velvetCakeIframe {
             display: none;
         }
         .header__upsellWrapper {
@@ -47,8 +35,8 @@
         .webiEmbeddedModule {
             display: none !important;
         }
-        .profileMenu__premium {
-            display: none !important;
+        .m-highlight {
+          display: none !important;
         }
     `;
     GM_addStyle(CSS);
