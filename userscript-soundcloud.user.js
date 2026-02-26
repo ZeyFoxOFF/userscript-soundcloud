243e1efee08e78fabd3b60784aea8fbb8980462c
ZeyFoxOFF <zeyfoxoff@gmail.com>
2026-02-26 18:45:18 +0100

    1.0.6

--- userscript-soundcloud.user.js
+++ userscript-soundcloud.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Hide SoundCloud Visual Ads
 // @namespace    https://github.com/ZeyFoxOFF/userscript-soundcloud
-// @version      1.0.5
+// @version      1.0.6
 // @description  Hide visual ads for soundcloud
 // @author       ZeyFox
 // @match        *://soundcloud.com/*
